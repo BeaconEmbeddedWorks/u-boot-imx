a0bd38e4cd689bd28dceb73b92e24b777f30956e
Adam Ford <aford173@gmail.com>
2020-01-22 14:24:35 -0600

    board: beacon: Add support for Beacon's imx8m Nano Kit
    Beacon EmbeddedWorks is launching a new development kit based
    on the i.MX8M Nano from NXP.
    
    This patch adds the device tree, board file and a script for
    sending the generated files to NXP's mkimage to burn to SD.
    
    Signed-off-by: Adam Ford <aford173@gmail.com>

---
+++ txfrToMkImg-N.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+cp ./arch/arm/dts/beacon-imx8mn-kit.dtb ../imx-mkimage/iMX8M/fsl-imx8mn-evk.dtb
+cp ./u-boot-nodtb.bin ../imx-mkimage/iMX8M
+cp ./u-boot-dtb.bin ../imx-mkimage/iMX8M
+cp ./spl/u-boot-spl.bin ../imx-mkimage/iMX8M
+cp ./spl/u-boot-spl-nodtb.bin ../imx-mkimage/iMX8M
+cd ../imx-mkimage
+make SOC=iMX8MN flash_hdmi_spl_uboot
+cd ../uboot-imx
+
