1365d429f46f9684850ed3188f5d079ef5e92e14
Adam Ford <aford173@gmail.com>
2020-07-16 07:42:57 -0500

    board: imx8m/n beacon: Fix scripts to generate proper flash.bin
    If compiling from uboot-imx, the user needs to go to ../imx-mkimage
    to find the flash.bin file.
    
    This patch adds a copy instruction to move the flash.bin file back
    to uboot when generated.
    
    Signed-off-by: Adam Ford <aford173@gmail.com>

--- txfrToMkImg-M.sh
+++ txfrToMkImg-M.sh
@@ -7,5 +7,6 @@ cp ./spl/u-boot-spl.bin ../imx-mkimage/iMX8M
 cp ./spl/u-boot-spl-nodtb.bin ../imx-mkimage/iMX8M
 cd ../imx-mkimage
 make SOC=iMX8MM flash_hdmi_spl_uboot
+cp iMX8M/flash.bin ../uboot-imx/.
 cd ../uboot-imx
 
