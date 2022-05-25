25d605754fcd4db9e5d72368deb0c2df2d9e0f2f
Adam Ford <aford173@gmail.com>
2022-05-25 06:30:22 -0500

    Revert "arm64: imx8mm_beacon: Update DTB if SoC is I-temp"
    This reverts commit 75644408878f0b592de94007507007f6b7cd874c.

--- board/beacon/imx8mm/imx8mm_beacon.c
+++ board/beacon/imx8mm/imx8mm_beacon.c
@@ -4,25 +4,10 @@
  */
 
 #include <asm/global_data.h>
-#include <env.h>
-#include <imx_thermal.h>
-#include <asm/mach-imx/sys_proto.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
-	return 0;
-}
-
-int board_late_init(void)
-{
-	int minc, maxc;
-
-	if (get_cpu_temp_grade(&minc, &maxc) == TEMP_INDUSTRIAL) {
-		env_set("trip0", "0x17318");
-		env_set("trip1", "0x19a28");
-	}
-
 	return 0;
 }
